b8f13f2a045d89a4053aab98e961c577eb091e52
Arun Isaac <arunisaac@systemreboot.net>
2024-01-12 01:33:11 +0000

    guix: Add G-expression computed-file to build website.
    * .guix/skribilo-package.scm: Import (guix profiles) and
    %guile-build-system-modules from (guix build-system guile).
    (skribilo-website-gexp, skribilo-website): New variables.

--- .guix/skribilo-package.scm
+++ .guix/skribilo-package.scm
@@ -1,7 +1,7 @@
 ;;; guix.scm  --  Build recipe for GNU Guix.
 ;;;
 ;;; Copyright © 2020 Ludovic Courtès <ludo@gnu.org>
-;;; Copyright © 2023 Arun Isaac <arunisaac@systemreboot.net>
+;;; Copyright © 2023–2024 Arun Isaac <arunisaac@systemreboot.net>
 ;;;
 ;;; This file is part of Skribilo.
 ;;;
@@ -23,9 +23,11 @@
   #:use-module ((gnu packages autotools) #:select (autoconf automake))
   #:use-module ((gnu packages gettext) #:select (gnu-gettext))
   #:use-module ((gnu packages skribilo) #:select (skribilo) #:prefix guix:)
+  #:use-module ((guix build-system guile) #:select (%guile-build-system-modules))
   #:use-module (guix gexp)
   #:use-module ((guix git-download) #:select (git-predicate))
   #:use-module (guix packages)
+  #:use-module (guix profiles)
   #:use-module (guix utils))
 
 (define S specification->package)
@@ -73,5 +75,68 @@
        (replace "guile" (S "guile@2.0"))))
     (native-inputs
      (package-native-inputs skribilo))))
+
+(define skribilo-website-gexp
+  (let ((development-profile
+         (profile
+          (content (package->development-manifest skribilo)))))
+    (with-imported-modules %guile-build-system-modules
+      #~(begin
+          (use-modules (guix build guile-build-system)
+                       (guix build utils))
+
+          (define (patch-shebangs)
+            (for-each patch-shebang
+                      (find-files "."
+                                  (lambda (file stat)
+                                    (eq? 'regular (stat:type stat))))))
+
+          (copy-recursively #$(package-source skribilo)
+                            (getcwd))
+          (set-path-environment-variable
+           "PATH" (list "/bin") (list #$development-profile))
+          (set-path-environment-variable
+           "LIBRARY_PATH" (list "/lib") (list #$development-profile))
+          (set-path-environment-variable
+           "ACLOCAL_PATH" (list "/share/aclocal") (list #$development-profile))
+          (set-path-environment-variable
+           "PKG_CONFIG_PATH" (list "/lib/pkgconfig") (list #$development-profile))
+          (set-path-environment-variable
+           "GUILE_LOAD_PATH"
+           (list (string-append "/share/guile/site/"
+                                (target-guile-effective-version)))
+           (list #$development-profile))
+          (set-path-environment-variable
+           "GUILE_LOAD_COMPILED_PATH"
+           (list (string-append "/lib/guile/" (target-guile-effective-version) "/site-ccache"))
+           (list #$development-profile))
+          (substitute* "doc/user/Makefile.am"
+            (("skribilo = [^\n]*")
+             (string-append "skribilo = " #$(file-append skribilo "/bin/skribilo"))))
+          (patch-shebangs)
+          (invoke "autoreconf" "--verbose" "--install" "--force")
+          (patch-shebangs)
+          (invoke "./configure" "CONFIG_SHELL=sh" "SHELL=sh")
+          (invoke "make"
+                  "--directory" "doc/user")
+          (invoke "make"
+                  (string-append "SKRIBILO=" #$skribilo "/bin/skribilo")
+                  "--directory" "web")
+          ;; Install website.
+          (for-each (lambda (file)
+                      (install-file file (string-append #$output "/doc")))
+                    (find-files "doc/user"
+                                (lambda (file stat)
+                                  (and (string=? (basename (dirname file)) "user")
+                                       (or (string-suffix? ".html" file)
+                                           (string-suffix? ".png" file))))))
+          (copy-recursively "doc/user/static"
+                            (string-append #$output "/doc/static"))
+          (install-file "web/index.html" #$output)
+          (copy-recursively "doc/user/static"
+                            (string-append #$output "/static"))))))
+
+(define-public skribilo-website
+  (computed-file "skribilo-website" skribilo-website-gexp))
 
 skribilo
